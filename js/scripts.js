55dddb2acd1d7089ae3418dd31d97dc502458ee1
FeloMatta <mattafelo@gmail.com>
2022-12-19 20:59:20 +0100

    Fine Esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,21 @@ let age = prompt("Inserisci l'età del passeggero:");
 console.log("Gli anni del passeggero sono:", age);
 
 // Calcola il prezzo base del biglietto in base ai km
-let price = parsefloat(distance) * 0.21;
+let price = parseFloat(distance) * 0.21;
+
+// Se il passeggero ha meno di 18 anni, applica lo sconto del 20%
+if (age < 18) {
+    price *= 0.8;
+}
+
+// Se il passeggero ha più di 65 anni, applica lo sconto del 40%
+else if (age >= 65) {
+    price *= 0.6;
+}
+console.log("prezzo del biglietto:€", price);
+
+// Formatta il prezzo finale con due decimali
+let formattedPrice = price.toFixed(2);
+
+// Mostra all'utente il prezzo del biglietto
+alert(`Il prezzo totale del viaggio è di ${formattedPrice} €`)
